778403aaf4a320e34ada22de41944125b0a10788
Daniel Adámek <daniel.adamek@tul.cz>
2023-04-06 09:39:08 +0200

    InvalidFileFormatException to public

--- src/main/java/cz/tul/alg2/semestral/exception/InvalidFileFormatException.java
+++ src/main/java/cz/tul/alg2/semestral/exception/InvalidFileFormatException.java
@@ -1,6 +1,6 @@
 package cz.tul.alg2.semestral.exception;
 
-class InvalidFileFormatException  extends Exception {
+public class InvalidFileFormatException  extends Exception {
     public InvalidFileFormatException (String str) {
         // calling the constructor of parent Exception
         super(str);
