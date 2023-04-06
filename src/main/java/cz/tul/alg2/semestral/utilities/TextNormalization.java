c5024ca71c5c98fa9c9f61b4f05e9bf4e916c991
Daniel Adámek <daniel.adamek@tul.cz>
2023-04-06 11:15:36 +0200

    text normalizer

---
+++ src/main/java/cz/tul/alg2/semestral/utilities/TextNormalization.java
@@ -0,0 +1,22 @@
+package cz.tul.alg2.semestral.utilities;
+
+public class TextNormalization {
+    public static String stringNormalize(String toNormalize) {
+        return toNormalize
+                .toLowerCase()
+                .replaceAll("[^ěščřžýáíéóúůďťňĎŇŤŠČŘŽÝÁÍÉÚŮĚÓa-zA-Z0-9]", "")
+                .replace('é', 'e')
+                .replace('í', 'i')
+                .replace('á', 'a')
+                .replace('ý', 'y')
+                .replace('ž', 'z')
+                .replace('ř', 'r')
+                .replace('č', 'c')
+                .replace('š', 's')
+                .replace('ě', 'e')
+                .replace('ť', 't')
+                .replace('ň', 'n')
+                .replaceAll("[ůú]", "u")
+                .replace('ó', 'o')
+                ; }
+}
