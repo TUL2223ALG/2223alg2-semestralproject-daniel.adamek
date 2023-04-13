d7425e2ebd3abfe6cb9649c596d11382755b0556
Daniel Adámek <daniel.adamek@tul.cz>
2023-04-13 08:29:32 +0200

    PathBuilder comments

--- src/main/java/cz/tul/alg2/semestral/utilities/PathBuilder.java
+++ src/main/java/cz/tul/alg2/semestral/utilities/PathBuilder.java
@@ -1,7 +1,16 @@
 package cz.tul.alg2.semestral.utilities;
 
-
+/**
+The PathBuilder class provides a static method to join an array of strings into a file system path
+using the system-specific path separator.
+*/
 public class PathBuilder {
+    /**
+     * Joins an array of strings into a file system path using the system-specific path separator.
+     *
+     * @param partsOfPath an array of strings representing the parts of the path
+     * @return a string representing the joined path
+     */
     public static String joinPath(String[] partsOfPath) {
         StringBuilder sb = new StringBuilder();
         String pathSeparator = System.getProperty("path.separator");
