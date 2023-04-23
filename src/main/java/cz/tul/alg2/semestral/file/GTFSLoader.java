4c8cfb1f1bde10cd5f450a616201d25e5f20aaa3
Daniel Adámek <daniel.adamek@tul.cz>
2023-04-23 22:42:59 +0200

    GTFSLoader optimization

--- src/main/java/cz/tul/alg2/semestral/file/GTFSLoader.java
+++ src/main/java/cz/tul/alg2/semestral/file/GTFSLoader.java
@@ -21,9 +21,6 @@ import java.util.HashMap;
 import java.util.List;
 
 public class GTFSLoader implements ILoader {
-
-    private final HashMap<String, Station> allStations = new HashMap<>();
-    private final HashMap<String, Line> allLines = new HashMap<>();
     public void loadFile(String path) {
         GtfsReader reader = new GtfsReader();
         GtfsDaoImpl dao = new GtfsDaoImpl();
@@ -35,30 +32,6 @@ public class GTFSLoader implements ILoader {
         } catch (IOException e) {
             new ErrorLogger("error.log").logError("Error while reading GTFS file", e);
         }
-        // Load all lines
-            /*
-            switch (route.getType()) {
-                case 0:
-                    yield TransportationType.TRAM;
-                case 1:
-                    yield TransportationType.METRO;
-                case 2:
-                    yield TransportationType.TRAIN;
-                case 3:
-                    yield TransportationType.BUS;
-                case 4:
-                    yield TransportationType.FERRY;
-                case 5:
-                    yield TransportationType.AIRPLANE;
-                case 6:
-                    yield TransportationType.TROLLEY;
-                case 7:
-                    yield TransportationType.CABLE_CAR;
-                default:
-                    new ErrorLogger("error.log").logError("WARNING: Invalid type of transportation! Skipping... Route:" + route.toString(), new IllegalArgumentException());
-                    yield TransportationType.NONE;
-            };
-            */
 
         for (Stop stop : dao.getAllStops()) {
             if (stop.getName() == null) continue;
@@ -70,20 +43,29 @@ public class GTFSLoader implements ILoader {
         }
 
         // Vytvoříme mapu tripId -> stopTimes
-        HashMap<String, List<StopTime>> tripStopTimesMap = new HashMap<>();
+        HashMap<String, List<StopTime>> tripStopTimesMap = new HashMap<>(80000);
         for (StopTime stopTime : dao.getAllStopTimes()) {
             tripStopTimesMap.computeIfAbsent(stopTime.getTrip().getId().getId(), k -> new ArrayList<>()).add(stopTime);
         }
 
         // Vytvoříme mapu routeId -> trips
-        HashMap<String, List<Trip>> routeTripsMap = new HashMap<>();
+        HashMap<String, List<Trip>> routeTripsMap = new HashMap<>(800);
         for (Trip trip : dao.getAllTrips()) {
             routeTripsMap.computeIfAbsent(trip.getRoute().getId().getId(), k -> new ArrayList<>()).add(trip);
         }
 
+        String shortName;
+        TransportationType lineType;
+        List<Trip> tripsForRoute;
+        List<StopTime> stopTimesForTrip;
+        List<Station> lineStations;
+        int travelTime;
+        StopTime stopTime, prevStopTime;
+        Station station, prevStation;
+        Pair<Station, Integer> neighbour, reverseNeighbour;
         for (Route route : dao.getAllRoutes()) {
-            String shortName = route.getShortName();
-            TransportationType lineType = switch (route.getType()) {
+            shortName = route.getShortName();
+            lineType = switch (route.getType()) {
                 case 0:
                     yield TransportationType.TRAM;
                 case 1:
@@ -105,27 +87,27 @@ public class GTFSLoader implements ILoader {
                     yield TransportationType.NONE;
             };
 
-            List<Trip> tripsForRoute = routeTripsMap.get(route.getId().getId());
+            tripsForRoute = routeTripsMap.get(route.getId().getId());
 
             for (Trip trip : tripsForRoute) {
-                List<StopTime> stopTimesForTrip = tripStopTimesMap.get(trip.getId().getId());
+                stopTimesForTrip = tripStopTimesMap.get(trip.getId().getId());
                 stopTimesForTrip.sort(Comparator.comparingInt(StopTime::getStopSequence));
-                List<Station> lineStations = new ArrayList<>();
+                lineStations = new ArrayList<>(stopTimesForTrip.size());
 
                 for (int i = 0; i < stopTimesForTrip.size(); i++) {
-                    StopTime stopTime = stopTimesForTrip.get(i);
-                    Station station = allStations.get(TextNormalization.stringNormalize(stopTime.getStop().getName()));
+                    stopTime = stopTimesForTrip.get(i);
+                    station = allStations.get(TextNormalization.stringNormalize(stopTime.getStop().getName()));
                     lineStations.add(station);
                     station.addRoute(new Line(shortName, lineType, lineStations));
 
                     if (i > 0) {
-                        StopTime prevStopTime = stopTimesForTrip.get(i - 1);
-                        Station prevStation = allStations.get(TextNormalization.stringNormalize(prevStopTime.getStop().getName()));
-                        int travelTime = stopTime.getArrivalTime() - prevStopTime.getDepartureTime();
+                        prevStopTime = stopTimesForTrip.get(i - 1);
+                        prevStation = allStations.get(TextNormalization.stringNormalize(prevStopTime.getStop().getName()));
+                        travelTime = (int) Math.ceil((double)(stopTime.getArrivalTime() - prevStopTime.getDepartureTime()) / 60);
 
-                        Pair<Station, Integer> neighbour = new Pair<>(prevStation, travelTime);
+                        neighbour = new Pair<>(prevStation, travelTime);
                         station.getNeighbours().add(neighbour);
-                        Pair<Station, Integer> reverseNeighbour = new Pair<>(station, travelTime);
+                        reverseNeighbour = new Pair<>(station, travelTime);
                         prevStation.getNeighbours().add(reverseNeighbour);
                     }
                 }
@@ -134,13 +116,5 @@ public class GTFSLoader implements ILoader {
                 allLines.put(shortName, line);
             }
         }
-    }
-
-    public HashMap<String, Station> getAllStations() {
-        return this.allStations;
-    }
-
-    public HashMap<String, Line> getAllLines() {
-        return this.allLines;
     }
 }
