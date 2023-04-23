76531725560c40fbd055d3371cacd2ddb6e39f37
Daniel Adámek <daniel.adamek@tul.cz>
2023-04-23 21:04:27 +0200

    TextLoader speed

--- src/main/java/cz/tul/alg2/semestral/file/TextLoader.java
+++ src/main/java/cz/tul/alg2/semestral/file/TextLoader.java
@@ -14,8 +14,8 @@ import java.util.HashMap;
 import java.util.List;
 
 public class TextLoader implements ILoader {
-    HashMap<String, Station> allStations = new HashMap<>();
-    HashMap<String, Line> allLines = new HashMap<>();
+    HashMap<String, Station> allStations = new HashMap<>(8000);
+    HashMap<String, Line> allLines = new HashMap<>(800);
     /**
      * Returns a list of file paths that end with ".txt" in the specified directory path.
      *
@@ -23,7 +23,7 @@ public class TextLoader implements ILoader {
      */
     @Override
     public void loadFile(String path) {
-        HashMap<Integer, Station> indexToStationMap = new HashMap<>();
+        HashMap<Integer, Station> indexToStationMap = new HashMap<>(allStations.size());
 
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
             String line;
@@ -31,51 +31,62 @@ public class TextLoader implements ILoader {
             boolean readingNeighbours = false;
             boolean readingLines = false;
 
+            String[] parts, neighbours, neighbourParts, stationIndexes;
+            String name, zoneID, lineName;
+            int index, neighbourIndex, travelTime;
+            Station station;
+            TransportationType lineType;
+
             while ((line = reader.readLine()) != null) {
-                if (line.equals("STATIONS")) {
-                    readingStations = true;
-                    readingNeighbours = false;
-                    readingLines = false;
-                } else if (line.equals("NEIGHBOURS")) {
-                    readingStations = false;
-                    readingNeighbours = true;
-                    readingLines = false;
-                } else if (line.equals("LINES")) {
-                    readingStations = false;
-                    readingNeighbours = false;
-                    readingLines = true;
-                } else {
-                    if (readingStations) {
-                        String[] parts = line.split("\\|");
-                        int index = Integer.parseInt(parts[0]);
-                        String name = parts[1];
-                        String zoneID = parts.length > 2 ? parts[2] : null;
-                        Station station = zoneID != null ? new Station(name, zoneID) : new Station(name);
-                        allStations.put(station.getName(), station);
-                        indexToStationMap.put(index, station);
-                    } else if (readingNeighbours) {
-                        String[] parts = line.split("\\|");
-                        if (parts.length < 2) continue;
-                        int stationIndex = Integer.parseInt(parts[0]);
-                        Station station = indexToStationMap.get(stationIndex);
-                        String[] neighbours = parts[1].split(";");
-                        for (String neighbour : neighbours) {
-                            String[] neighbourParts = neighbour.split(",");
-                            int neighbourIndex = Integer.parseInt(neighbourParts[0]);
-                            int travelTime = Integer.parseInt(neighbourParts[1]);
-                            station.addNeighbour(new Pair<>(indexToStationMap.get(neighbourIndex), travelTime));
+                switch (line) {
+                    case "STATIONS" -> {
+                        readingStations = true;
+                        readingNeighbours = false;
+                        readingLines = false;
+                    }
+                    case "NEIGHBOURS" -> {
+                        readingStations = false;
+                        readingNeighbours = true;
+                        readingLines = false;
+                    }
+                    case "LINES" -> {
+                        readingStations = false;
+                        readingNeighbours = false;
+                        readingLines = true;
+                    }
+                    default -> {
+                        if (readingStations) {
+                            parts = line.split("\\|");
+                            index = Integer.parseInt(parts[0]);
+                            name = parts[1];
+                            zoneID = parts.length > 2 ? parts[2] : null;
+                            station = zoneID != null ? new Station(name, zoneID) : new Station(name);
+                            allStations.put(station.getName(), station);
+                            indexToStationMap.put(index, station);
+                        } else if (readingNeighbours) {
+                            parts = line.split("\\|");
+                            if (parts.length < 2) continue;
+                            index = Integer.parseInt(parts[0]);
+                            station = indexToStationMap.get(index);
+                            neighbours = parts[1].split(";");
+                            for (String neighbour : neighbours) {
+                                neighbourParts = neighbour.split(",");
+                                neighbourIndex = Integer.parseInt(neighbourParts[0]);
+                                travelTime = Integer.parseInt(neighbourParts[1]);
+                                station.addNeighbour(new Pair<>(indexToStationMap.get(neighbourIndex), travelTime));
+                            }
+                        } else if (readingLines) {
+                            parts = line.split("\\|");
+                            lineName = parts[0];
+                            lineType = TransportationType.valueOf(parts[1]);
+                            stationIndexes = parts[2].split(",");
+                            List<Station> lineStations = new ArrayList<>(stationIndexes.length);
+                            for (String stationIndex : stationIndexes) {
+                                lineStations.add(indexToStationMap.get(Integer.parseInt(stationIndex)));
+                            }
+                            Line newLine = new Line(lineName, lineType, lineStations);
+                            allLines.put(lineName, newLine);
                         }
-                    } else if (readingLines) {
-                        String[] parts = line.split("\\|");
-                        String lineName = parts[0];
-                        TransportationType lineType = TransportationType.valueOf(parts[1]);
-                        String[] stationIndexes = parts[2].split(",");
-                        List<Station> lineStations = new ArrayList<>();
-                        for (String stationIndex : stationIndexes) {
-                            lineStations.add(indexToStationMap.get(Integer.parseInt(stationIndex)));
-                        }
-                        Line newLine = new Line(lineName, lineType, lineStations);
-                        allLines.put(lineName, newLine);
                     }
                 }
             }
