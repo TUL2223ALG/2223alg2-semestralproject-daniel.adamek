c5e1f6d015d1444ef678083405ecf6d7ae903fd8
Daniel Adámek <daniel.adamek@tul.cz>
2023-04-24 09:47:49 +0200

    TextNormalization 10x increased speed

--- src/main/java/cz/tul/alg2/semestral/utilities/TextNormalization.java
+++ src/main/java/cz/tul/alg2/semestral/utilities/TextNormalization.java
@@ -1,22 +1,43 @@
 package cz.tul.alg2.semestral.utilities;
 
+import java.util.HashMap;
+
 public class TextNormalization {
+    private final static StringBuilder sb = new StringBuilder();
+    //private static int[] map = new int[383];
+    private final static HashMap<Integer, Integer> map = new HashMap<>();
+    static {
+            map.put((int) 'é', (int) 'e');
+            map.put((int) 'ě', (int) 'e');
+            map.put((int) 'š', (int) 's');
+            map.put((int) 'č', (int) 'c');
+            map.put((int) 'ř', (int) 'r');
+            map.put((int) 'ž', (int) 'z');
+            map.put((int) 'ý', (int) 'y');
+            map.put((int) 'á', (int) 'a');
+            map.put((int) 'í', (int) 'i');
+            map.put((int) 'ť', (int) 't');
+            map.put((int) 'ň', (int) 'n');
+            map.put((int) 'ó', (int) 'o');
+            map.put((int) 'ú', (int) 'u');
+            map.put((int) 'ů', (int) 'u');
+    }
     public static String stringNormalize(String toNormalize) {
-        return toNormalize
-                .toLowerCase()
-                .replaceAll("[^ěščřžýáíéťňóúůa-zA-Z0-9]", "")
-                .replace('é', 'e')
-                .replace('í', 'i')
-                .replace('á', 'a')
-                .replace('ý', 'y')
-                .replace('ž', 'z')
-                .replace('ř', 'r')
-                .replace('č', 'c')
-                .replace('š', 's')
-                .replace('ě', 'e')
-                .replace('ť', 't')
-                .replace('ň', 'n')
-                .replaceAll("[ůú]", "u")
-                .replace('ó', 'o')
-                ; }
+        sb.setLength(0);
+
+        int c, mapped;
+        for (int i = 0; i < toNormalize.length(); i++) {
+            c = toNormalize.charAt(i);
+            if (c >= 0x30 && c <= 0x39)
+                continue;
+            c = Character.toLowerCase(c);
+            if (c >= 0x61 && c <= 0x7A)
+                sb.appendCodePoint(c);
+            else {
+                mapped = map.get(c);
+                sb.appendCodePoint(mapped);
+            }
+        }
+        return sb.toString();
+    }
 }
--- src/test/java/cz/tul/alg2/semestral/utilities/TextNormalizationTest.java
+++ src/test/java/cz/tul/alg2/semestral/utilities/TextNormalizationTest.java
@@ -2,6 +2,8 @@ package cz.tul.alg2.semestral.utilities;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TextNormalizationTest {
@@ -44,4 +46,55 @@ class TextNormalizationTest {
         b = "nechtjizhrisnesaxofonyablurozezvucisinudesnymitonywaltzutangaaquickstepu";
         assertEquals(TextNormalization.stringNormalize(a), b);
     }
+
+
+    @Test
+    void testStringNormalizePerformance() {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ĚŠŘŽÝÁÍÉÚŤŇÓěščřžýáíéúůťňó";
+        for (int i = 0; i < 100_000_000; i++) {
+            sb.append(alphabet.charAt(random.nextInt(alphabet.length())));
+        }
+        String input = sb.toString();
+        String newString;
+        long startTime = System.nanoTime();
+        newString = TextNormalization.stringNormalize(input);
+        long endTime = System.nanoTime();
+
+        double elapsedTime = (endTime - startTime) / 1_000_000_000.0;
+        System.out.printf("Elapsed time: %.3f seconds%n", elapsedTime);
+
+        StringBuilder expected = new StringBuilder(input.length());
+
+        startTime = System.nanoTime();
+        for (char c : input.toLowerCase().toCharArray()) {
+            switch (c) {
+                case 'ě', 'é' -> expected.append('e');
+                case 'š' -> expected.append('s');
+                case 'č' -> expected.append('c');
+                case 'ř' -> expected.append('r');
+                case 'ž' -> expected.append('z');
+                case 'ý' -> expected.append('y');
+                case 'á' -> expected.append('a');
+                case 'í' -> expected.append('i');
+                case 'ť' -> expected.append('t');
+                case 'ň' -> expected.append('n');
+                case 'ó' -> expected.append('o');
+                case 'ú', 'ů' -> expected.append('u');
+                default -> {
+                    if (Character.isLetter(c)) {
+                        expected.append(c);
+                    }
+                }
+            }
+        }
+        endTime = System.nanoTime();
+
+        elapsedTime = (endTime - startTime) / 1_000_000_000.0;
+        System.out.printf("Old method elapsed time: %.3f seconds%n", elapsedTime);
+
+        assertEquals(expected.toString(), newString);
+    }
+
 }
