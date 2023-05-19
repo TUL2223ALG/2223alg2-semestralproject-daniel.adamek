2b6c34009c7c4fa6b14cb36f3601037706a4d58d
Daniel Adámek <daniel.adamek@tul.cz>
2023-05-19 22:34:09 +0200

    Loading + line searching debug

--- src/main/java/cz/tul/alg2/semestral/userinterface/LoaderSelector.java
+++ src/main/java/cz/tul/alg2/semestral/userinterface/LoaderSelector.java
@@ -5,9 +5,13 @@ import cz.tul.alg2.semestral.file.GTFSLoader;
 import cz.tul.alg2.semestral.file.ILoader;
 import cz.tul.alg2.semestral.file.TextLoader;
 import cz.tul.alg2.semestral.utilities.PathBuilder;
+import cz.tul.alg2.semestral.utilities.TextNormalization;
 
 import java.io.File;
-import java.time.*;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Scanner;
 
 public class LoaderSelector {
@@ -19,43 +23,28 @@ public class LoaderSelector {
      *
      * @return A loader object
      */
-    public static ILoader getLoaderMethod() {
+    public static ILoader getLoaderMethod(boolean possibleReturn) {
         Scanner sc = new Scanner(System.in);
         String str;
 
-        LocalDateTime now = LocalDateTime.now(), lastEdited;
-        Duration age;
-        System.out.println("Zadejte cestu k souboru, ze kterého chcete načíst MHD: \n - \"*.ser\" pro binární soubory\n - \"*.txt\" pro textové soubory\n - \"*.zip\" pro GTFS formát.");
-        System.out.println("------------------------------------------------------------------------");
-        System.out.println("| 3 možné testovací varianty:                                          |");
-
-        File file = new File(PathBuilder.joinPath("data", "pid.txt"));
-        if (file.exists()) {
-            lastEdited = Instant.ofEpochMilli(file.lastModified()).atZone(ZoneId.systemDefault()).toLocalDateTime();
-            age = Duration.between(lastEdited, now);
-            System.out.printf("|   \"data/pid.txt\"       (rychlé)  Naposledy upraveno: %d dní, %02d:%02d:%02d |\n", age.toDays(), age.toHoursPart(), age.toMinutesPart(), age.toSecondsPart());
-        }
-
-        file = new File(PathBuilder.joinPath("data", "pid.txt"));
-        if (file.exists()) {
-            lastEdited = Instant.ofEpochMilli(file.lastModified()).atZone(ZoneId.systemDefault()).toLocalDateTime();
-            age = Duration.between(lastEdited, now);
-            System.out.printf("|   \"data/pid.ser\"       (rychlé)  Naposledy upraveno: %d dní, %02d:%02d:%02d |\n", age.toDays(), age.toHoursPart(), age.toMinutesPart(), age.toSecondsPart());
-        }
-
-        file = new File(PathBuilder.joinPath("data", "pid.txt"));
-        if (file.exists()) {
-            lastEdited = Instant.ofEpochMilli(file.lastModified()).atZone(ZoneId.systemDefault()).toLocalDateTime();
-            age = Duration.between(lastEdited, now);
-            System.out.printf("|   \"data/PID_GTFS.zip\"  (pomalé)  Naposledy upraveno: %d dní, %02d:%02d:%02d |\n", age.toDays(), age.toHoursPart(), age.toMinutesPart(), age.toSecondsPart());
-        }
-        System.out.println("------------------------------------------------------------------------");
         while (true) {
+            System.out.println(
+                    """
+                    Zadejte cestu k souboru, ze kterého chcete načíst MHD.
+                     - "*.ser" pro binární soubory
+                     - "*.txt" pro textové soubory
+                     - "*.zip" pro GTFS formát.""");
+            if (possibleReturn) System.out.println("Pro opuštění menu, napište ZPĚT");
+
+            suggestActualFiles();
             System.out.print("> ");
             str = sc.nextLine();
+
+            if (TextNormalization.stringNormalize(str).equals("zpet") && possibleReturn) return null;
+
             // exists?
             if (str.matches("^(.+/)*.+\\.(ser|txt|zip)$")) {
-                if (! new File(str).exists()) {
+                if (!new File(str).exists()) {
                     System.out.println("Soubor na takovéto cestě neexistuje.  Formát: cesta/k/souboru");
                     continue;
                 }
@@ -63,14 +52,12 @@ public class LoaderSelector {
                 System.out.println("Neplatný formát. Zadejte znovu. Formát: povolené koncovky: .ser, .txt a .zip");
                 continue;
             }
-            ILoader loader = null;
+            ILoader loader;
             if (str.matches("^(.+/)*.+\\.ser$")) {
                 loader = new BinaryLoader();
-            }
-            else if (str.matches("^(.+/)*.+\\.txt$")) {
+            } else if (str.matches("^(.+/)*.+\\.txt$")) {
                 loader = new TextLoader();
-            }
-            else if (str.matches("^(.+/)*.+\\.zip$")) {
+            } else if (str.matches("^(.+/)*.+\\.zip$")) {
                 loader = new GTFSLoader();
             } else {
                 System.out.println("Neplatný formát. Zadejte znovu. Formát: povolené koncovky: .ser, .txt a .zip");
@@ -84,5 +71,40 @@ public class LoaderSelector {
                 )
             ) return loader;
         }
+    }
+
+    /**
+     * The suggestActualFiles function is a static function that prints out the three possible test files
+     * that can be used to run the program.
+     */
+    private static void suggestActualFiles() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime lastEdited;
+        Duration age;
+
+        System.out.println("-------------------------------------------------------------------------");
+        System.out.println("| 3 možné testovací varianty:                                           |");
+        File file = new File(PathBuilder.joinPath("data", "pid.txt"));
+        if (file.exists()) {
+            lastEdited = Instant.ofEpochMilli(file.lastModified()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+            age = Duration.between(lastEdited, now);
+            System.out.printf("|   \"data/pid.txt\"       (rychlé)  Naposledy upraveno: %2d dní, %02d:%02d:%02d |%n", age.toDays(), age.toHoursPart(), age.toMinutesPart(), age.toSecondsPart());
+        }
+
+        file = new File(PathBuilder.joinPath("data", "pid.ser"));
+        if (file.exists()) {
+            lastEdited = Instant.ofEpochMilli(file.lastModified()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+            age = Duration.between(lastEdited, now);
+            System.out.printf("|   \"data/pid.ser\"       (rychlé)  Naposledy upraveno: %2d dní, %02d:%02d:%02d |%n", age.toDays(), age.toHoursPart(), age.toMinutesPart(), age.toSecondsPart());
+        }
+
+        file = new File(PathBuilder.joinPath("data", "PID_GTFS.zip"));
+        if (file.exists()) {
+            lastEdited = Instant.ofEpochMilli(file.lastModified()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+            age = Duration.between(lastEdited, now);
+            System.out.printf("|   \"data/PID_GTFS.zip\"  (pomalé)  Naposledy upraveno: %2d dní, %02d:%02d:%02d |%n", age.toDays(), age.toHoursPart(), age.toMinutesPart(), age.toSecondsPart());
+        }
+        System.out.println("-------------------------------------------------------------------------");
+
     }
 }
--- src/main/java/cz/tul/alg2/semestral/userinterface/Menu.java
+++ src/main/java/cz/tul/alg2/semestral/userinterface/Menu.java
@@ -36,8 +36,27 @@ public class Menu {
                 loader.getAllStations(),
                 loader.getAllLines()
         );
+    }
+
+    /**
+     * The Menu function is the constructor for the Menu class.
+     * It loads all the data from a menu.
+     */
+    public Menu() {
+        do {
+            loadTransportMenu(false);
+            if (this.loader == null) System.out.println("Je nutné, načíst nějaká data!");
+        } while (this.loader == null);
         this.ig = new InteractiveGetter(this.transport, this.sc);
     }
+
+    /**
+     * The getTransport function returns the transport object of a CityTransport.
+     */
+    public CityTransport getTransport() {
+        return transport;
+    }
+
     /**
      * The mainMenu function is the main menu of the program.
      * It allows you to choose between searching for a path,
@@ -47,28 +66,20 @@ public class Menu {
         String str;
         while (true) {
             System.out.println(
-                    "Vyberte jednu z možností:\n" +
-                    "H    Hledat cestu\n" + // TODO
-                    "HU   Hledat cestu s možností uložení výpisu\n" + // TODO
-                    "P    Prohledávat stanice a linky (menu)\n" +
-                    "N    Načíst nové stanice\n" +
-                    "V    Zkontrolovat validitu souboru\n" + // TODO
-                    "O    Opustit program");
+                    """
+                    Vyberte jednu z možností:
+                    H    Hledat cestu
+                    HU   Hledat cestu s možností uložení výpisu
+                    P    Prohledávat stanice a linky (menu)
+                    N    Načíst nové stanice
+                    V    Zkontrolovat validitu souboru
+                    O    Opustit program""");
             System.out.print("> ");
             str = sc.nextLine().toLowerCase();
             switch (str) {
                 case "h" -> findPathMenu();
                 case "p" -> transportViewerMenu();
-                case "n" -> {
-                    this.loader = LoaderSelector.getLoaderMethod();
-                    System.out.println(
-                            "Načteno " + this.loader.getAllStations().size() +
-                            " stanic a " + this.loader.getAllLines().size() + " linek." );
-                    this.transport = new CityTransport(
-                            loader.getAllStations(),
-                            loader.getAllLines()
-                    );
-                }
+                case "n" -> loadTransportMenu(true);
                 case "o", "opustit" -> {
                     System.out.println("Opouštíte program.");
                     return;
@@ -78,6 +89,29 @@ public class Menu {
 
         }
     }
+
+    /**
+     * The loadDataMenu function is used to load the data from a file.
+     * It uses the LoaderSelector class to determine which loader should be used, and then loads all the stations
+     * and lines into a CityTransport object.
+     */
+    public void loadTransportMenu(boolean possibleReturn) {
+        ILoader newLoader = LoaderSelector.getLoaderMethod(possibleReturn);
+        if (newLoader == null) {
+            System.out.println("Nic nového nebylo načteno.");
+            return;
+        }
+
+        this.loader = newLoader;
+        System.out.println(
+                "Načteno " + this.loader.getAllStations().size() +
+                " stanic a " + this.loader.getAllLines().size() + " linek." );
+        this.transport = new CityTransport(
+                loader.getAllStations(),
+                loader.getAllLines()
+        );
+    }
+
     private void findPathMenu() {
         Station from, to;
         int tmpCharCounter, lineCharCounter, totalTravelTime = 0;
@@ -267,7 +301,7 @@ public class Menu {
 
             System.out.println("---------------------------[PROHLÍŽEČ LINEK]----------------------------");
             System.out.println(
-                    "Jméno: " + line.getName() +
+                    "Jméno: " + line.getPrettyName() +
                     "\nDruh dopravy: " + line.getLineType() +
                     "\nStanice: \n" + sb.toString()
             );
